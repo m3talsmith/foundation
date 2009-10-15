0b9062b30084941f95d06d727cba22dd3f010660
Michael Christenson II <m3talsmith@gmail.com>
2009-10-14 23:49:37 -0400

    fixed a typo on line 31 - the source for the required gem was missing http://

--- config/environment.rb
+++ config/environment.rb
@@ -28,7 +28,7 @@ Rails::Initializer.run do |config|
   config.gem 'liquid'
   config.gem 'haml', :version => '>= 2.2.0'
   config.gem 'compass', :version => '>= 0.8.5', :source => 'http://gemcutter.org'
-  config.gem 'grid-coordinates', :lib => 'compass', :version => '>= 1.0.3', :source => 'gemcutter.org'
+  config.gem 'grid-coordinates', :lib => 'compass', :version => '>= 1.0.3', :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
